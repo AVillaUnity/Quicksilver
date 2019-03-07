5765027a133f843985d14fe371ecbbb42b731f48
Alvaro Villa <alvarovilla1@outlook.com>
2019-03-07 12:03:35 -0500

    Initial commit

---
+++ Assets/BlowUpBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlowUpBehavior : StateMachineBehaviour
+{
+    public GameObject particleBlowUp;
+    public float slowDown = .3f;
+    public float slowDownSpeed = .5f;
+    public float musicFadeInSpeed = 1f;
+
+    private AudioSource audioSource;
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Instantiate(particleBlowUp, animator.transform.position, Quaternion.identity);
+        animator.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        audioSource = animator.GetComponent<AudioSource>();
+
+        audioSource.Play();
+    }
+
+    //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(Time.timeScale > slowDown)
+        {
+            Time.timeScale = Mathf.Clamp(Time.timeScale - Time.unscaledDeltaTime * slowDownSpeed, 0f, 1f);
+            Time.fixedDeltaTime = Time.timeScale * 0.01f;
+        }
+
+        if(audioSource.volume < 1)
+        {
+            audioSource.volume += (Time.unscaledDeltaTime * musicFadeInSpeed);
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ Assets/Scripts/QuicksilverController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class QuicksilverController : MonoBehaviour
+{
+
+    public float speed = 100f;
+    public float sensitivity = 1f;
+    public float gravity = 9.81f;
+    public float initialJumpForce = 10f;
+
+    [Range(0f, 1f)]
+    public float slowDownTime = 0f;
+    public Transform head;
+
+    private Rigidbody rb;
+    private CharacterController characterController;
+    private float jumpForce = 0f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
+
+        ToggleTime();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ToggleTime();
+        }
+    }
+
+    private void Movement()
+    {
+        float forwardMovement = Input.GetAxisRaw("Vertical");
+        float sidewaysMovement = Input.GetAxisRaw("Horizontal");
+
+        float horizontalRotation = Input.GetAxisRaw("Mouse X");
+        float verticalRotation = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 movement = ((transform.forward * forwardMovement) + (transform.right * sidewaysMovement)) * speed * Time.unscaledDeltaTime;
+        if (characterController.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                jumpForce = initialJumpForce;
+            }
+        }
+        else
+        {
+            jumpForce -= gravity * Time.unscaledDeltaTime;
+        }
+        movement.y -= (gravity * Time.unscaledDeltaTime) - (jumpForce * Time.unscaledDeltaTime);
+        characterController.Move(movement);
+
+        transform.Rotate(0f, sensitivity * horizontalRotation, 0f);
+        head.Rotate(-verticalRotation * sensitivity, 0f, 0f);
+    }
+
+    void ToggleTime()
+    {
+        Time.timeScale = (Time.timeScale < 1) ? 1f : slowDownTime;
+    }
+}
