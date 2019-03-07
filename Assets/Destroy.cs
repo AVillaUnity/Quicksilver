eb76b768d224921aa65e6f554c462df78f3d5254
Alvaro Villa <alvarovilla1@outlook.com>
2019-03-07 14:38:21 -0500

    Concept

---
+++ Assets/Destroy.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, 5f * Time.timeScale);
+    }
+}
---
+++ Assets/Scripts/BreakFloor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class BreakFloor : MonoBehaviour
+{
+    public GameObject breakParticles;
+    public float timeScale = .03f;
+    public Transform[] footPositions;
+    // Start is called before the first frame update
+    public void DestroyFloor()
+    {
+        if(Time.timeScale <= timeScale)
+        {
+            Vector3 positionToSpawnIn = footPositions[0].position;
+            foreach(Transform t in footPositions)
+            {
+                if(t.position.y < positionToSpawnIn.y)
+                {
+                    positionToSpawnIn = t.position;
+                    break;
+                }
+            }
+            Instantiate(breakParticles, positionToSpawnIn, Quaternion.identity);
+        }
+    }
+}
